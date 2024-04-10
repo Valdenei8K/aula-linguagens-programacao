e13db83ac0e80c2b5cb21f829c6752187862a8e1
Valdenei8K <valdenei.etech@gmail.com>
2024-04-09 21:37:26 -0300

    nome ano concat
    codigo que faz um print concatenado do nome e do ano

---
+++ nome_e_nascimento.py
@@ -0,0 +1,9 @@
+nome = input("Digite seu nome :")
+sobrenome = input("Digite seu sobrenome :")
+ano_de_nascimento = input("em que ano voce nasceu?: ")
+nasceu =("nasceu no ano de  ")
+
+resultado = nome.lower()+"  " + sobrenome.upper()+" "  + nasceu + ano_de_nascimento
+
+print(resultado)  
+
